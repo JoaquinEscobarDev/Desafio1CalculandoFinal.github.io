b2d76f3f631e1412293d5f439092bc9a879ba28c
JoaquinEscobarDev <joaquin.escobar.dev@gmail.com>
2025-10-04 17:38:41 -0300

    Refactor HTML structure and enhance CSS styles; implement JavaScript functionality for product quantity management and total price calculation.

--- js/index.js
+++ js/index.js
@@ -1,4 +1,34 @@
-precio = 400000
+const precioBase = 400000;
 
-precioSpan = document.querySelector(".precio-inicial");
-precioSpan.innerHTML = precio
+//formato CL
+const precioSpan = document.querySelector("#precio-inicial");
+precioSpan.textContent = precioBase.toLocaleString("es-CL");
+
+const btnSumar = document.querySelector("#btn-sumar");
+const btnRestar = document.querySelector("#btn-restar");
+const cantidad = document.querySelector("#cantidad");
+const total = document.querySelector("#precio-total");
+
+//cantidad
+let cantidadProductos = parseInt(cantidad.textContent);
+
+//actualizar total
+function actualizarTotal() {
+  total.textContent = (precioBase * cantidadProductos).toLocaleString("es-CL");
+}
+
+//sumar
+btnSumar.onclick = function () {
+  cantidadProductos++;
+  cantidad.textContent = cantidadProductos;
+  actualizarTotal();
+};
+
+//restar
+btnRestar.onclick = function () {
+  if (cantidadProductos > 0) {
+    cantidadProductos--;
+    cantidad.textContent = cantidadProductos;
+    actualizarTotal();
+  }
+};
